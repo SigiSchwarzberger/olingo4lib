05d3258fcb2efacd4755b9dba07ac7ce551c23ab
a-schwasie <a-schwasie@vieaps006.mp.local>
2026-03-28 13:33:56 +0100

    initial commit

---
+++ src/main/java/sigi/olingo4lib/MyTester.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package sigi.olingo4lib;
+
+/**
+ * @author a-schwasie
+ *
+ */
+public class MyTester {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+			System.out.println("Hello World!");
+
+	}
+
+}
